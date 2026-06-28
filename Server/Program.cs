49c9ff4b2d78128c1c88df1d1c13f538aa255b36
Vinicius Ferreira Citadin <viniciuscitadin@gmail.com>
2026-06-28 19:40:39 -0300

    atualizado inicialização do servidor

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,25 +1,23 @@
+using Server.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
+app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
+
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+app.MapGrpcService<ImageService>().EnableGrpcWeb();
+
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.MapGrpcReflectionService();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
