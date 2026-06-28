c1e3a35a2e0345d8882577a1ad1860cd3cb7d3e1
Vinicius Ferreira Citadin <viniciuscitadin@gmail.com>
2026-06-28 19:40:02 -0300

    implementado serviço para processamento de imagens no servidor

---
+++ Server/Services/ImageService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using Server;
+
+namespace Server.Services
+{
+    public class ImageService : Server.ImageService.ImageServiceBase
+    {
+        private readonly ILogger<ImageService> _logger;
+        
+        public ImageService(ILogger<ImageService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ApplyFilterResponse> ApplyFilter(ApplyFilterRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation($"Aplicando filtro do tipo {request.FilterCase}");
+
+            return Task.FromResult(new ApplyFilterResponse
+            {
+                Image = request.Image
+            });
+        }
+    }
+}
